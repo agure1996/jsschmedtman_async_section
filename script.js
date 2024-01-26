34c629b96e13966a427f93c66c18d252af98e9a6
greggbaker <abbasgure@googlemail.com>
2024-01-26 20:40:36 +0000

    progress

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+/*  gonna make ajax call - the old school way */
+
+const req = new XMLHttpRequest();
+//https://countries-api-836d.onrender.com/countries/
+req.open('GET','https://restcountries.com/v2/name/portugal');
+req.send();
+// console.log(req.responseText);
+
+req.addEventListener('load',(()=>{
+    console.log(this.responseText);
+}))
