662d36d81d7d02f460087b2122ad95f3544a790d
greggbaker <abbasgure@googlemail.com>
2024-02-22 16:45:50 +0000

    fixed call undefined issue

--- script.js
+++ script.js
@@ -5,6 +5,33 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+
+
+//render country
+
+const renderCountry = function(data){
+      
+  const html = `
+  <article class="country">
+  <img class="country__img" src="${Object.values(data.flags)[1]}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} Million People </p>
+    <p class="country__row"><span>🗣️</span>${Object.values(data.languages).join(", ")}</p>
+    <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+  </div>
+</article>
+  `;
+
+  //insert the html block at
+  countriesContainer.insertAdjacentHTML('beforeend',html)
+  //set opacity to 100%
+  countriesContainer.style.opacity = 1;
+}
+
+
+
 /*  gonna make ajax call - the old school way */
 
 
@@ -12,40 +39,38 @@ const getCountryData = function(country){
     
     const req = new XMLHttpRequest();
     //https://countries-api-836d.onrender.com/countries/
-    req.open('GET', `https://restcountries.eu/rest/v3.1/name/${country}`);
+    req.open('GET', `https://restcountries.com/v3.1/name/${country}`);
     req.send();
 
 
 
     req.addEventListener('load', function () {
         const [data] = JSON.parse(this.responseText);
-        console.log(data);
+       
+        //used to test debug
+        // console.log(Object.values(data.flags)[1]);
+        console.log(data)
+        renderCountry(data)
+
       
+        const [neighbour] = data.borders;
+        if(!neighbour) return;
+        else 
+
+        req.open('GET', `https://restcountries.com/v3.1/name/${neighbour}`);
+        req.send();
+        neighbour = JSON.parse(this.responseText);
+       
+        //used to test debug
+        // console.log(Object.values(data.flags)[1]);
+        console.log(neighbour)
+        renderCountry(neighbour)
       
-      
-      
-      
-        const html = `
-          <article class="country">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} Million People </p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
-            <p class="country__row"><span>💰</span>${data.currencies}</p>
-          </div>
-        </article>
-          `;
-      
-          //insert the html block at
-          countriesContainer.insertAdjacentHTML('beforeend',html)
-          //set opacity to 100%
-          countriesContainer.style.opacity = 1;
       });
 
 }
 
-getCountryData('Portugal')
-getCountryData('Spain')
+
+getCountryData('somalia')
+
 
